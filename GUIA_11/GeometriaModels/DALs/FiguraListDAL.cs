7a2cb3341c97e11aaf836932a00e01ff641119a0
adlfvrr <adolfover4@gmail.com>
2025-10-02 15:21:16 -0300

    -

---
+++ GUIA_11/GeometriaModels/DALs/FiguraListDAL.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeometriaModels.Models;
+
+namespace GeometriaModels.DALs;
+
+public class FiguraListDAL:IFiguraDAL
+{
+    List<FiguraModel> figuras = new();
+    int Id = 1;
+
+    public FiguraModel Add(FiguraModel figura)
+    {
+        if (figura.Id > 0) return null;
+
+        figura.Id = Id++;
+        figuras.Add(figura);
+        return figura;
+    }
+
+    public List<FiguraModel> GetALL()
+    {
+        return figuras;
+    }
+
+    public FiguraModel GetById(int id)
+    {
+        /*return (from f in figuras
+                 where f.Id == id
+                 select f).FirstOrDefault();*/
+        return figuras.Where(p => p.Id == id).FirstOrDefault();
+    }
+
+    public bool Remove(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    public FiguraModel Update(FiguraModel entity)
+    {
+        throw new NotImplementedException();
+    }
+
+
+}
---
+++ GUIA_11/GeometriaModels/DALs/IFiguraDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeometriaModels.Models;
+
+namespace GeometriaModels.DALs;
+public interface IFiguraDAL
+{
+    public List<FiguraModel> GetALL();
+    public FiguraModel GetById(int id);
+    public FiguraModel Add(FiguraModel figura);
+    public bool Remove(int id);
+    public FiguraModel Update(FiguraModel entity);
+}
